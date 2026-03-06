34edb9b0e4d2b72fdaf85455f142b03ed3edd4e0
thesangpers-svg <thesangpers@gmail.com>
2026-03-06 11:55:08 +0700

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://server-quanlykho.onrender.com/api";
 
 export const loginUser = async (data) => {
   const res = await axios.post(`${API_URL}/login`, data);
